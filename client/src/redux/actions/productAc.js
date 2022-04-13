818a9cccca6efb08c475f1400dd987f002449e62
Roman <romakubak@gmail.com>
2022-04-13 21:44:43 +0300

    мои товары и удаление

--- client/src/redux/actions/productAc.js
+++ client/src/redux/actions/productAc.js
@@ -19,10 +19,31 @@ export const getProduct = () => async(dispatch) =>{
   .then((response) => {
    dispatch(setProduct(response.data))
   })
-
 }
+
 export const getFilterProducts = (product) => async(dispatch) => {
-   
-    dispatch(setProduct(product))
- 
+  dispatch(setProduct(product))
+}
+
+export const setMyProducts = (product) => {
+  return {type: 'SET_MY_PRODUCT', payload: product}
+}
+
+export const allMyProducts = (id) => async(dispatch) => {
+  axios.get(`http://localhost:3001/product/allMyProduct/${id}`)
+    .then( (res) => {
+      dispatch(setMyProducts(res.data))
+    })
+}
+//---------asdfasdf---------------
+export const deleteOneProd = (productId) => {
+  return {type: 'REMOVE_PRODUCT', payload: productId}
+}
+
+export const deleteProduct = (productId) => async(dispatch) =>{
+  console.log('productId', productId);
+  axios.post(`http://localhost:3001/product/delete/${productId}`)
+    .then((data) => {
+      dispatch(deleteOneProd(productId))
+    })
 }
