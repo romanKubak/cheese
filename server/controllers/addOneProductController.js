e9fc531030cfa6334f98bf40bbfc8e8110be0a3a
Roman <romakubak@gmail.com>
2022-04-14 18:08:51 +0300

    done

--- server/controllers/addOneProductController.js
+++ server/controllers/addOneProductController.js
@@ -1,18 +1,19 @@
 const uuid = require('uuid')
 const path = require('path')
-const { User, Product, Category } = require('../db/models')
+const { User, Product, Category, SubCategory } = require('../db/models')
 
 class AddOneProductController {
   async create (req, res) {
     try {
-      const { name, description, price, category, userId } = req.body
-      const thisCategory = await Category.findOne({where: { name: category }})
+      const { name, description, price, subCategory, userId } = req.body
+      console.log(subCategory);
+      const thisSubCategory = await SubCategory.findOne({where: { id: subCategory }})
       const { img } = req.files
       let fileName = uuid.v4() + ".jpg"
       img.mv(path.resolve(__dirname, '..', 'static', fileName))
   
       const product = await Product
-        .create({name, description, price, img: fileName, category_id: thisCategory.id, seller_id: userId});
+        .create({name, description, price, img: fileName, subCategory_id: thisSubCategory.id, seller_id: userId});
       return res.json(product)
     } catch (error) {
       console.log(error);
