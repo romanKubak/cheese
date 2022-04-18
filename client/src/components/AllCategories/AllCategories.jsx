57ff5b33509880eb35bcd3d5682a7456880d7bc0
Roman <romakubak@gmail.com>
2022-04-18 15:38:20 +0300

    немного стилей

--- client/src/components/AllCategories/AllCategories.jsx
+++ client/src/components/AllCategories/AllCategories.jsx
@@ -11,20 +11,11 @@ const add = (id) => {
   return (
     <div>
       <div className={styles.main_box} onClick={()=> add(category.id)}>
-      <div className="card" style={{width: '18rem'}}>
+      <div className="cards" style={{width: '18rem'}}>
         <img src={process.env.REACT_APP_API_URL + category.img} className={styles.card_img_top} alt="..."/>
-        <div className="card-body">
+        <div className={styles.card_body}>
           <h5 className="card-title">{category.name}</h5>
-         
         </div>
-        <ul className="list-group list-group-flush">
-          
-        
-        {/* <button className="btn btn-primary" onClick={()=> add(category.id)}>Подробнее</button> */}
-            
-         
-         
-        </ul>
       </div>
     </div>
     </div>
