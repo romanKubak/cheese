9cb4ad0323a9f4c12ca9b20a9da3264d89e28994
Max <burnashkin02@mail.ru>
2022-04-19 20:23:21 +0900

    dev

--- client/src/components/HelloPage/HelloPage.jsx
+++ client/src/components/HelloPage/HelloPage.jsx
@@ -14,24 +14,25 @@ export default function HelloPage() {
     <div >
     <div className={styles.container}>
         <div className={styles.marvel}>
-        <div style={{color: 'rgba(183, 60, 55, 1)'}}>MARVEL</div>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-        <div style={{color: 'rgba(243, 243, 243, 1)'}}>&#8211;</div>
-        <div style={{color: 'rgba(51, 116, 234, 1)'}}> DC</div>
-        </div>
-        <div style={{color: 'rgba(243, 243, 243, 1)', paddingLeft: '450px'}}>STAR WARS</div>
+        <p style={{color: 'rgba(183, 60, 55, 1)',textAlign: 'left', paddingLeft: '100px'}}>MARVEL</p>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <span style={{color: 'rgba(243, 243, 243, 1)', paddingLeft: '110px'}}>&#8211;</span>
+        <p style={{ color: 'rgba(51, 116, 234, 1)',textAlign: 'left', paddingLeft: '120px'}}>
+         DC</p>
+         </div>
+        <p style={{color: 'rgba(243, 243, 243, 1)', paddingLeft: '280px'}}>STAR WARS</p>
         </div>
         <div style={{paddingRight:'80px'}}>
-          <img src = {process.env.REACT_APP_API_URL + 'photo_2022-04-18_21-23-11.jpg'} style={{width:'700px', height:'413px',paddingTop:'50px'}}></img>
+          <img src = {process.env.REACT_APP_API_URL + 'photo_2022-04-18_21-23-11.jpg'} style={{width:'700px', height:'370px',paddingTop:'30px'}}></img>
         </div>
         </div>
       <div className={styles.harry}>
         
-      <div style={{color: 'rgba(244, 204, 63, 1)'}}> HARRY POTTER</div>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-      <div style={{color: 'rgba(243, 243, 243, 1)'}}>И МНОГОЕ </div>
-      <div style={{marginLeft:'30px', color: 'rgba(64, 189, 121, 1)'}}> ДРУГОЕ</div>
-      </div>
+      <p style={{color: 'rgba(244, 204, 63, 1)',textAlign: 'left', paddingLeft: '100px'}}> HARRY POTTER</p>
+      <p style={{marginLeft:'30px', color: 'rgba(64, 189, 121, 1)',textAlign: 'center',marginRight:'250px'}}>
+      <span style={{color: 'rgba(243, 243, 243, 1)'}}>И МНОГОЕ </span>
+      ДРУГОЕ
+      </p>
         </div>
     
       <Button style={style} variant="contained" onClick={() => redir()}>Перейти к покупкам</Button>
