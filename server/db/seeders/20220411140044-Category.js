3988f736b0f6ef2d120f997047238df3da99c33f
Roman <romakubak@gmail.com>
2022-04-21 16:46:26 +0300

    сиды

--- server/db/seeders/20220411140044-Category.js
+++ server/db/seeders/20220411140044-Category.js
@@ -5,37 +5,37 @@ module.exports = {
      await queryInterface.bulkInsert('Categories', [
       {
         name: 'Marvel',
-        img: 'logo-marvel-1.jpeg',
+        img: 'MARVEL.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'DC - Detective Comics',
-        img: 'DC_Comics_logo.png',
+        img: 'DC.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Гарри Поттер',
-        img: 'Harry-Potter-Logo.png',
+        img: 'HARRY_POTER.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Форсаж',
-        img: 'fors_logo.jpeg',
+        img: 'FORSAJ.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Звездные войны',
-        img: 'star-wars-logo.png',
+        img: 'STAR_WARS.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: 'Дополнительные товары',
-        img: 'other.png',
+        img: 'ANY.png',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
